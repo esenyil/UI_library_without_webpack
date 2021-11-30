5b028ddd7e77685226f2505ebc85685812336815
Esen <esenyildirim88@gmail.com>
2021-11-30 23:20:15 +0100

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+});
+
+//create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+//create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('you clicked me :)')
+});
